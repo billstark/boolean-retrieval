c585d8901914fa555acbacdb2cbd3b64ad36bb13
Yang Zhuohan <billstark1996@gmail.com>
2018-02-11 20:50:32 +0800

    finish indexing file

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import re
 import nltk
 import sys
 import getopt
+import math
 
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
@@ -28,3 +29,74 @@ for o, a in opts:
 if input_directory == None or output_file_postings == None or output_file_dictionary == None:
     usage()
     sys.exit(2)
+
+ps = nltk.stem.PorterStemmer()
+temp_list = []
+dictionary_file = open(output_file_dictionary, 'w')
+posting_file = open(output_file_postings, 'w')
+
+for index in range(1, 14818):
+    input_file_name = input_directory + str(index)
+    print "trying to index file " + str(index) + "\n"
+    try:
+        input_file = open(input_file_name, 'r')
+        input_file_content = input_file.read()
+
+        for sentence in nltk.sent_tokenize(input_file_content):
+            words = map(lambda word: ps.stem(word).lower(), nltk.word_tokenize(sentence))
+            for word in words:
+                if [word, index] in temp_list:
+                    continue
+                temp_list.append([word, index])
+
+        input_file.close()
+    except IOError as e:
+        continue
+
+temp_list.sort(key = lambda doc: doc[1])
+temp_list.sort(key = lambda doc: doc[0])
+
+processed_list = {}
+word_list = []
+index = 0
+for [word, doc_id] in temp_list:
+    if word not in processed_list:
+        word_list.append(word)
+        processed_list[word] = {
+            'index': index,
+            'posting': [doc_id]
+        }
+        index = index + 1
+        continue
+    processed_list[word]['posting'].append(doc_id)
+
+
+
+offset = 0
+for word in word_list:
+    posting = processed_list[word]['posting']
+    skip = int(math.sqrt(len(posting)))
+    next_index = 0
+    posting_list = ""
+    for index, doc_id in enumerate(posting):
+        if index == next_index:
+            if index + skip >= len(posting):
+                posting_list = posting_list + str(doc_id) + ":" + str(len(posting) - 1) + " "
+                continue
+            next_index = next_index + skip
+            posting_list = posting_list + str(doc_id) + ":" + str(next_index) + " "
+            continue
+        posting_list = posting_list + str(doc_id) + ":" + str(index + 1) + " "
+    posting_list = posting_list[:len(posting_list) - 1] + "\n"
+    processed_list[word]['offset'] = offset
+    offset = offset + len(posting_list)
+    posting_file.write(posting_list)
+
+posting_file.close()
+
+for word in word_list:
+    dictionary_file.write(word + " " + str(processed_list[word]['offset']) + " " + str(len(processed_list[word]['posting'])) + "\n")
+
+dictionary_file.close()
+
+
