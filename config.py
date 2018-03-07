63fe8f5f9383102cd5fdac33b3bafe87a000a23b
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-03-07 16:02:02 +0800

    Add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+import re
+
+INVALID_CHARS = re.compile(r'[^\w\d\s]+')
+INVALID_QUERY_CHARS = re.compile(r'[^\w\d\s()]+')
+OPERATORS = {
+    'OR': 1,
+    'AND': 2,
+    'NOT': 3,
+    '(': 4,
+    ')': 4,
+}
